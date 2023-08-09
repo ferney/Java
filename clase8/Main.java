17396c9d8377d740813f25c532da911f0b507f5e
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2023-08-09 10:36:08 -0500

    Add files via upload

---
+++ clase8/Main.java
@@ -0,0 +1,44 @@
+package org.example.B1.clase8;
+
+import javax.swing.*;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean continuar = true;
+        while (continuar) {
+            try {
+                int opcion=Integer.parseInt(JOptionPane.showInputDialog(
+                        "Menú:\n"
+                        +"1. Realizar división\n"
+                        +"2. Salir\n"
+                        +"Seleccione una opción: "
+                ));
+               switch (opcion) {
+                    case 1 -> realizarDivision();
+                    case 2 -> continuar = false;
+                    default -> mensaje("Opción no válida. Intente de nuevo.");
+                }
+            } catch (NumberFormatException e) {
+                mensaje("Error: Ingrese un valor numérico.");
+            }
+        }
+    }
+    private static void realizarDivision() {
+        try {
+            double numerador =  Double.parseDouble(JOptionPane.showInputDialog("Ingrese el primer numerador:"));
+            double denominador = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el denominador:"));
+            double resultado = numerador / denominador;
+            mensaje("El resultado de la división es: " + resultado);
+        } catch (ArithmeticException e) {
+            mensaje("Error: División indeterminada.");
+        } catch (NumberFormatException e) {
+            mensaje("Error: Ingrese un valor numérico válido.");
+        }catch (NullPointerException e) {
+            mensaje("Error: Ingrese un valor numérico válido.");
+        }
+    }
+    private static void mensaje(String x) {
+        JOptionPane.showMessageDialog(null,x);
+    }
+}
